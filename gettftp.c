4f725bfced48ee6671fea30fae863f3718c03449
MatteoEnsea <matt.doffe@gmail.com>
2024-12-12 09:05:13 +0100

    Commit via console

---
+++ gettftp.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <netdb.h>
+
+int main(int argc,char *argv[]){
+    if (argc != 3) {
+        fprintf(stderr, "Missing argument\n");
+        exit(EXIT_FAILURE); 
+    }
+
+    struct addrinfo hints;
+    struct addrinfo *res;
+    memset(&hints,0,sizeof(struct addrinfo));
+
+    getaddrinfo(argv[1],NULL,&hints, &res);
+    struct addrinfo *current = res;
+}
