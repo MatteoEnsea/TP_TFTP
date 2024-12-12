a667d0ebee816bad7877ac45721828bc7378535e
MatteoEnsea <matt.doffe@gmail.com>
2024-12-12 11:49:48 +0100

    Question 2 (avec commentaire)

--- gettftp.c
+++ gettftp.c
@@ -9,15 +9,17 @@
 #include <netdb.h>
 
 int main(int argc,char *argv[]){
-       if (argc != 4) {
+       if (argc != 4) { //Verify the arguments
         fprintf(stderr, "Usage %s <host> <port> <filename>\n",argv[0]);
         exit(EXIT_FAILURE); 
     }
 
+    //Question 1 : (get the arguments)
     const char *host = argv[1];
     const char *port = argv[2];
     const char *file_name = argv[3];
 
+    //Set hints to get UDP server only with IPV4
     struct addrinfo hints, *res;
     memset(&hints,0,sizeof(hints));
 
@@ -25,11 +27,13 @@ int main(int argc,char *argv[]){
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_protocol = IPPROTO_UDP;
 
-    if (getaddrinfo(argv[1],argv[2],&hints, &res) != 0){
+    //Question 2 :
+    if (getaddrinfo(argv[1],argv[2],&hints, &res) != 0){ //Verify if error while using getaddrinfo
         perror("Error while getaddrinfo");
         exit(EXIT_FAILURE);
     };
 
+    //Verify that we found the only one UDP server
     struct addrinfo *current = res;
     while (current->ai_next != NULL){
         printf("Found one result");
