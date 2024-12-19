503ddc549646e5d71a6a1127bef1daef1fcf43ee
MatteoEnsea <matt.doffe@gmail.com>
2024-12-19 09:21:10 +0100

    Q3 et réglages Q2

--- gettftp.c
+++ gettftp.c
@@ -1,43 +1,49 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <unistd.h>
 #include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
 int main(int argc,char *argv[]){
-       if (argc != 4) { //Verify the arguments
-        fprintf(stderr, "Usage %s <host> <port> <filename>\n",argv[0]);
-        exit(EXIT_FAILURE); 
-    }
+
+    if (argc != 4) {//Error if not enough args
+       fprintf(stderr, "Usage: %s <host> <port> <filename>\n", argv[0]);
+       return 1;
+   }
 
     //Question 1 : (get the arguments)
     const char *host = argv[1];
     const char *port = argv[2];
-    const char *file_name = argv[3];
+    const char *filename = argv[3];
 
-    //Set hints to get UDP server only with IPV4
+    //Set hints to get UDP server only with TCP and IPV4 
     struct addrinfo hints, *res;
-    memset(&hints,0,sizeof(hints));
-
+    memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = IPPROTO_UDP;
 
     //Question 2 :
-    if (getaddrinfo(argv[1],argv[2],&hints, &res) != 0){ //Verify if error while using getaddrinfo
-        perror("Error while getaddrinfo");
+    if (getaddrinfo(host, port, &hints, &res) != 0) { 
+       perror("Erreur de résolution DNS");
+       exit(1);
+   }
+
+
+    //Question 3 :
+    int sock;
+
+    //If the socket isnt a connection IPV4 and TCP
+    if (sock = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP)<0){
+        perror("socket error");
         exit(EXIT_FAILURE);
-    };
+    }
 
     //Verify that we found the only one UDP server
-    struct addrinfo *current = res;
-    while (current->ai_next != NULL){
-        printf("Found one result");
-        printf("\t ai_family = %d\n",current->ai_family);
-        current = current->ai_next;
-        }
+    struct addrinfo *current;
+    for (current = res; current != NULL; current = current->ai_next) {
+       printf("Famille : %d, Type : %d, Protocole : %d\n",current->ai_family, current->ai_socktype, current->ai_protocol);
+   }
 }
